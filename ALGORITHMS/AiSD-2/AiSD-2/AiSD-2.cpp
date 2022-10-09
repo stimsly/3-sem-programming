99c7f10f175e129111f73278d3a85be60e3e92d7
stimsly <valaignat@gmail.com>
2022-10-09 23:00:56 +0300

    som went wrong -> fix

--- ALGORITHMS/AiSD-2/AiSD-2/AiSD-2.cpp
+++ ALGORITHMS/AiSD-2/AiSD-2/AiSD-2.cpp
@@ -12,7 +12,7 @@ struct p {
     int x, y;
 }a[100000], b[100000];
 
-bool is_ans_exist = 0;
+bool is_ans_exist = 1;
 int total_ans = 9999999;
 
 bool sortf(p a, p b) {
@@ -54,13 +54,13 @@ int find(int l, int r, p (&a)[100000]) {
         a[i] = b[i];
       //  cout << a[i].x << " " << a[i].y << endl;
     }
-   // cout << "H: " << h << endl << a[(l+r)/2].x << endl;
+    //cout << "H: " << h << endl << a[(l+r)/2].x << endl;
     vector <p> B;
-    //cout << "B:\n";
+   // cout << "B:\n";
     for (int i = l; i < r; i++) {
-        if (abs(a[(l + r) / 2].x - a[i].x) <= h) {
+        if (abs(a[(l + r) / 2].x - a[i].x) * abs(a[(l + r) / 2].x - a[i].x) <= h) {
             B.push_back(a[i]);
-      //      cout << a[i].x << " " << a[i].y << endl;
+        //    cout << a[i].x << " " << a[i].y << endl;
         }
     }
 
@@ -69,21 +69,28 @@ int find(int l, int r, p (&a)[100000]) {
     for (int i = 0; i < B.size(); i++) {
         int x = 0;
         for (int j = i + 1; j < B.size(); j++) {
-            if (abs(B[i].y - B[j].y) <= h) {
+            if (abs(B[i].y - B[j].y) * abs(B[i].y - B[j].y) <= h) {
                 ans = min(ans, dist(B[i], B[j]));
-                total_ans = min(ans, total_ans);
-                if (total_ans == ans) x++;
+                if (total_ans > ans) {
+                    x = 1;
+                    total_ans = ans;
+                }
+                else if (total_ans == ans) x++;
             }
             else break;
         }
         for (int j = i - 1; j >= 0; j--) {
-            if (abs(B[i].y - B[j].y) <= h) {
+            if (abs(B[i].y - B[j].y) * abs(B[i].y - B[j].y) <= h) {
                 ans = min(ans, dist(B[i], B[j]));
-                total_ans = min(ans, total_ans);
-                if (total_ans == ans) x++;
+                if (total_ans > ans) {
+                    x = 1;
+                    total_ans = ans;
+                }
+                else if (total_ans == ans) x++;
             }
             else break;
         }
+        //cout << total_ans << " " << x << " " << B[i].x << " " << B[i].y << endl;
         if (x > 1) {
             is_ans_exist = 0;
         }
@@ -114,7 +121,7 @@ int main()
         cout << "Total ans in sqr: ";
         cout << (total_ans + .0);
         cout << "\nTotal ans: ";
-        cout << sqrt(total_ans * 1.0 / 2);
+        cout << sqrt(total_ans * 1.0);
     }
     else {
         cout << "Ответа нет";
